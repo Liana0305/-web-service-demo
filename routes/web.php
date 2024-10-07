6389961d9d2f8d3b454ec59f6ff9f2406864ce07
Liana0305 <anaanalagi79@gmail.com>
2024-10-07 12:31:09 +0700

    Initial commit - portofolio

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Route::get('/portpolio', function () { 
+    return view('portpolio'); 
+    });
+Route::get('/about', function () { 
+    return view('about'); 
+});
+Route::get('/hello', function () { 
+    return view('hello'); 
+    });
+Route::get('/', function () {
+    return view('welcome');
+});
