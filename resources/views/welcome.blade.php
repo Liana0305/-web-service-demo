8387d3984541cb664f7424ad785dae606589520c
Liana0305 <anaanalagi79@gmail.com>
2024-10-14 12:33:14 +0700

    Update navbar with new links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,18 +11,17 @@
 href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> 
 </head> 
 <body> 
-    <!-- Navbar --> 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark"> 
-        <a class="navbar-brand" href="#">My Laravel Site</a> 
-        <div class="collapse navbar-collapse"> 
-            <ul class="navbar-nav ml-auto"> 
-                <li class="nav-item"><a class="nav-link" href="/">Home</a></li> 
-                <li class="nav-item"><a class="nav-link" href="/about">About</a></li> 
-                <li class="nav-item"><a class="nav-link" href="/portfolio">Portfolio</a></li> 
-                <li class="nav-item"><a class="nav-link" href="/contact">Contact</a></li> 
-            </ul> 
-        </div> 
-    </nav> 
+    
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark"> 
+<a class="navbar-brand" href="#">My Laravel Site</a> 
+<div class="collapse navbar-collapse"> 
+<ul class="navbar-nav ml-auto"> 
+<li class="nav-item"><a class="nav-link" href="/">Home</a></li> 
+<li class="nav-item"><a class="nav-link" href="/about">About</a></li> 
+<li class="nav-item"><a class="nav-link" href="/contact">Contact</a></li> 
+</ul> 
+</div> 
+</nav> 
  
 <!-- Main Content --> 
 <div class="container"> 
