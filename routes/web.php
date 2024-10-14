abc4cc29e2cbddca9bb7cda1ec7069beac4f93bf
Liana0305 <anaanalagi79@gmail.com>
2024-10-14 12:11:11 +0700

    Add contact page with form

--- routes/web.php
+++ routes/web.php
@@ -25,12 +25,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/contact', function () {
-    return view('contact');
+Route::get('/contact', function () { 
+    return view('contact'); 
 });
 
 Route::post('/contact', function (Illuminate\Http\Request $request) {
     // Logika untuk menangani pengiriman form kontak
     return back()->with('success', 'Your message has been sent!');
 });
+
 
