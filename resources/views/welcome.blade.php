2378cdf04b3e253f0c8f56a4e3a58c46a20cc5be
Liana0305 <anaanalagi79@gmail.com>
2024-10-14 12:36:37 +0700

    Add contact page and navbar link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,17 +11,8 @@
 href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> 
 </head> 
 <body> 
-    
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark"> 
-<a class="navbar-brand" href="#">My Laravel Site</a> 
-<div class="collapse navbar-collapse"> 
-<ul class="navbar-nav ml-auto"> 
-<li class="nav-item"><a class="nav-link" href="/">Home</a></li> 
-<li class="nav-item"><a class="nav-link" href="/about">About</a></li> 
-<li class="nav-item"><a class="nav-link" href="/contact">Contact</a></li> 
-</ul> 
-</div> 
-</nav> 
+
+@include('navbar')
  
 <!-- Main Content --> 
 <div class="container"> 
